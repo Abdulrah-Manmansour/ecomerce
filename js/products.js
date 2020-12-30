35c9eb45b9bc9d5a7869d2f316048ab478f184d9
Abdulrahman-boop <abdulrahmanmansour600@gmail.com>
2020-12-30 13:19:27 +0200

    coment4

--- js/products.js
+++ js/products.js
@@ -22,13 +22,24 @@
       cursorborderradius: "10px", 
       scrollspeed: 20,
     });
+    $('#cardmeune').niceScroll({
+      cursorcolor: "#74b9ff",
+      cursorwidth: "10px",
+      zindex: "888888",
+      cursoropacitymin: '0.5',
+      cursorborderradius: "10px", 
+      scrollspeed: 20,
+    });
      //water animation
      $(".imgwater100").ripples({
       dropRadius: 10,
       perturbance: 0.08,
       });
 });
- 
+ var btndcarde=$("#deletcard");
+btndcarde.on('click',function(){
+  $(".btndelet").fadeOut();
+})
  //slide togale list menue
  $('.fa-cart-plus').on('click',function(){
   $("#cardmeune").slideToggle();
@@ -51,69 +62,79 @@ $('.btn-card').on('click',function(){
    let icon7 = $('.fa7');
    let icon8 = $('.fa8');
    let icon9 = $('.fa9');
+   let deletCard=$(".btndelet") 
    //append item and fadein the icon
    $('#b1').on('click', function () {
      icon1.fadeIn(1000);
+     deletCard.fadeIn();
    var nameProduct=$('#itemcard1').children('.content').children('h5').text();
    var btn = "<button class='btn buynow'> Buy Now </button>";
    var price=$('#itemcard1').children('.content').children('.price').text();
-    $("#cardmeune").append("<li>"  +`<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+    $("#last-item").append("<li>"  +`<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b2').on('click', function () {
      icon2.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard2').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard2').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b3').on('click', function () {
      icon3.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard3').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard3').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b4').on('click', function () {
      icon4.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard4').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard3').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b5').on('click', function () {
      icon5.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard5').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard5').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b6').on('click', function () {
      icon6.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard6').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard6').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b7').on('click', function () {
      icon7.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard7').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard7').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b8').on('click', function () {
      icon8.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard8').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard8').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
    $('#b9').on('click', function () {
      icon9.fadeIn(1000);
+     deletCard.fadeIn();
      var nameProduct=$('#itemcard9').children('.content').children('h5').text();
      var btn = "<button class='btn buynow'> Buy Now </button>";
      var price=$('#itemcard9').children('.content').children('.price').text();
-     $("#cardmeune").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
+     $("#last-item").append("<li>" + `<span class="hlist">${nameProduct}</span>` +   "&#09; - &#09;"  + `<span class="spanprice">${price}$</span>`  + btn +"<li>");
    });
 //show button top and hide
 $(window).scroll(function () {
@@ -140,10 +161,15 @@ new WOW().init();
  
 //add to card
 var noite = document.getElementById('h4card');
-var btn = document.getElementsByClassName('btn-card')
 let count = 0;
 function clc() {
   count += 1;
-  noite.classList.add('zero')
+  noite.classList.add('zero');
   noite.innerHTML = count;
 }
+//btndelet card
+btnCard=document.getElementById("deletcard");
+listCard=document.getElementById("last-item")
+btnCard.addEventListener('click',function(){
+listCard.innerHTML='';
+});
